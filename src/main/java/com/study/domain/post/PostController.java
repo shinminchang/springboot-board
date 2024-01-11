550d8651bf64f8c1eeac42b9db641857b5898f62
shinminchang <tlsalsckd01@naver.com>
2024-01-11 15:59:47 +0900

    게시판 리스트 게시판 상세 추가

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class PostController {
@@ -27,5 +29,21 @@ public class PostController {
     public String savePost(final PostRequest params) {
         postService.savePost(params);
         return "redirect:/post/list.do";
+    }
+
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+
+        return "post/list";
+    }
+
+    @GetMapping("/post/view.do")
+    public String openPostView(@RequestParam final Long id, Model model) {
+        PostResponse post = postService.findPostById(id);
+        model.addAttribute("post", post);
+
+        return "post/view";
     }
 }
